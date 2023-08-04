b3b270a98a0d726a1dcd01a99d72e280b00f9af6
Sofia Hojberg <sofiahojberg@gmail.com>
2023-08-03 22:28:31 -0300

    Change products list

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../database/productos.json')));
+
 const controller = {
     index: (req, res) => {
         res.render('index')
@@ -18,8 +23,18 @@ const controller = {
         res.render('productDetail')
     },
     categories: (req, res) => {
-        res.render('categories')
+        const categoriaSeleccionada = req.params.categoria;
+        const categoriaEncontrada = productos.find(categoria => categoria.categoria === categoriaSeleccionada)
+        tituloCategoria = formatearNombreCategoria(categoriaEncontrada.categoria)
+        const productosCategoria = categoriaEncontrada.productos
+        res.render('categories', {categoria: tituloCategoria, productos: productosCategoria})
     }
+}
+
+function formatearNombreCategoria(categoria) {
+    const palabras = categoria.replace(/([A-Z])/g, ' $1').trim().split(' ');
+    const palabrasCapitalizadas = palabras.map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1));
+    return palabrasCapitalizadas.join(' ');
 }
 
 module.exports = controller
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -10,6 +10,6 @@ router.get('/signup', mainController.signup);
 router.get('/newProducts', mainController.newProducts);
 router.get('/productCart', mainController.productCart);
 router.get('/productDetail', mainController.productDetail);
-router.get('/categories', mainController.categories);
+router.get('/categories/:categoria', mainController.categories);
 
 module.exports = router;
